f7f71c216ea10238eeaa26e2d918f3d08bd3e1a3
Mark <zilberhleitmark@mail.ru>
2024-03-06 09:30:20 +0300

    added branch frontend and tested readme file

--- YAFADEI/settings.py
+++ YAFADEI/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = os.getenv("DEBUG") == "True" or True
 
-# '127.0.0.1'
+# '127.0.0.1' 
 ALLOWED_HOSTS = []
 
 

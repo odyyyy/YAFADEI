2e1e636e31f1149b7e83015efed98d253bbb6527
odyyyy <iliya.odyvan4ikys@gmail.com>
2024-03-10 23:30:14 +0300

    added form for writing posts

--- posts/models.py
+++ posts/models.py
@@ -3,4 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    pass
+    title = models.CharField(max_length=255, verbose_name="Заголовок поста")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    img = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name="Фото")
+    slug = models.SlugField(max_length=255, db_index=True, unique=True, verbose_name='URL')
+    likes = models.IntegerField(default=0, verbose_name='Лайки')
+    published_time = models.DateTimeField(auto_now_add=True,verbose_name='Время публикации')
+
+    def __str__(self):
+        return self.title
+

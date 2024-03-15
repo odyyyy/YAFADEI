d969b9d23011a20293e1007668512c1b46829e78
odyyyy <iliya.odyvan4ikys@gmail.com>
2024-03-16 01:00:45 +0300

    rewrite with ClassViews

--- posts/views.py
+++ posts/views.py
@@ -1,36 +1,42 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
 
 from YAFADEI.settings import LOGIN_URL
 from posts.forms import AddPostForm
+from posts.models import Posts
 from posts.services import add_post_info_to_db, get_posts_list_from_db, get_post_from_db
 
 
 def homepage_view(request):
     return render(request, 'pages/index.html')
 
+
 @login_required(login_url=LOGIN_URL)
 def add_new_post_view(request):
     if request.method == 'POST':
         add_post_info_to_db(request)
-        return redirect('homepage')
+        return redirect('posts_page')
     return render(request, 'pages/posts/add_post.html', {
         'form': AddPostForm
     })
 
 
-def show_posts_view(request):
-    posts_list = get_posts_list_from_db()
-    return render(request, 'pages/posts/posts.html', {"posts_list": posts_list})
+class AllPostsPageView(ListView):
+    template_name = 'pages/posts/posts.html'
+    context_object_name = 'posts_list'
+    queryset = get_posts_list_from_db()
 
 
-def post_page_view(request, post_slug: str):
-    post = get_post_from_db(post_slug)
-    return render(request, 'pages/posts/post_page.html', {'post': post})
+class PostPageView(DetailView):
+    model = Posts
+    template_name = 'pages/posts/post_page.html'
+    context_object_name = 'post'
 
 
 def handle404Error(request, exception):
     return render(request, 'pages/errors/404.html')
+
 
 def handle500Error(request):
     return render(request, 'pages/errors/500.html')

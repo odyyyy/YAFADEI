1023297aa65a6dad20798524362b84adff1dd71c
odyyyy <iliya.odyvan4ikys@gmail.com>
2024-03-16 01:39:25 +0300

    add test for homepage_view

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.utils.text import slugify
 
-# Create your tests here.
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+
+from users.models import User
+from .models import Posts
+
+
+class ViewTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.post = Posts.objects.create(title='Test Post', content='Test Content', slug=slugify('Test Post'), user=self.user)
+
+    def test_homepage_view(self):
+        response = self.client.get(reverse('homepage'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/index.html')
+

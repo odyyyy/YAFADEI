0e8ab11ea0ec7a7e51ca03591108228737973c9e
odyyyy <iliya.odyvan4ikys@gmail.com>
2024-03-17 22:41:33 +0300

    Ðchange static dir setting

--- YAFADEI/settings.py
+++ YAFADEI/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [BASE_DIR / "static/css", ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

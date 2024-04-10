4e77e960db4a00a6214e47b4f555ef30d05f3d66
odyyyy <iliya.odyvan4ikys@gmail.com>
2024-04-10 22:26:31 +0300

    add models for favorite posts and subscribers

--- users/models.py
+++ users/models.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import AbstractUser, Permission
 from django.db import models
 
+from posts.models import Posts
+
 
 class User(AbstractUser):
     """Расширенная модель пользователя"""
 
-    favorite_posts = models.JSONField(default=list)
     groups = models.ManyToManyField(
         "auth.Group", related_name="custom_user_set"
     )
@@ -17,3 +18,19 @@ class User(AbstractUser):
         verbose_name = "Пользователи"
         verbose_name_plural = "Пользователи"
         ordering = ["id"]
+
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Подписчик"
+    )
+    subscribed_to = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Подписан на"
+    )
+
+
+class FavoritePost(models.Model):
+    user = models.ForeignKey(
+        User, related_name="favorite_posts", on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
